e33c65a86ba7ec5582251a3a8242d8cd0bf05687
Rodi <Rodi@DESKTOP-485M5RC>
2023-03-05 19:37:13 +0100

    Projektdateien hinzufügen.

---
+++ F1 game launcher/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace F1_game_launcher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
